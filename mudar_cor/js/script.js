3dc679784badbbe04a803d560456196a76598b21
Pedro Lucas <pedrolucastpetali@hotmail.com>
2024-09-06 11:17:03 -0300

    second commit

---
+++ mudar_cor/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('verde').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = 'green';
+});
+
+document.getElementById('amarelo').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = 'yellow';
+});
+
+document.getElementById('roxo').addEventListener('mouseover', function() {
+    document.body.style.backgroundColor = 'purple';
+});
